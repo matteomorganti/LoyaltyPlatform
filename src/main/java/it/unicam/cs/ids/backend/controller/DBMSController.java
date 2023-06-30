131214a01cf055e5773fd7546c36f6a186f47804
Matteo Morganti <matteo.morganti@studenti.unicam.it>
2023-06-30 23:32:35 +0200

    Add database connection for PostgreSQL
    Establish a connection to a PostgreSQL database. The class provides a static method called "getConnection," which returns a Connection object to interact with the specified database.

--- src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
@@ -1,4 +1,15 @@
 package it.unicam.cs.ids.backend.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBMSController {
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/LoyaltyPlatform";
+        String user = "pogres";
+        String password = "password";
+        return DriverManager.getConnection(url, user, password);
+    }
 }
