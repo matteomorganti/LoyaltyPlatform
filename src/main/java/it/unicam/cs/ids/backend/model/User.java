ec737979af20a972ce2f63c9e99d2200e4f8aef9
__andrei98 <andreisebastia.stana@studenti.unicam.it>
2023-06-30 23:37:48 +0200

    Implement User model class
    The User class represents a user in the loyalty platform system and contains various attributes such as id, name, surname, address, email, username, password, and telephone.

--- src/main/java/it/unicam/cs/ids/backend/model/User.java
+++ src/main/java/it/unicam/cs/ids/backend/model/User.java
@@ -1,4 +1,91 @@
 package it.unicam.cs.ids.backend.model;
 
+import java.util.Objects;
+
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String address;
+    private String email;
+    private int telephone;
+    private String username;
+    private String password;
+
+    public User(String name, String surname, String address, String email, String username, String password, int telephone) {
+        this.id=randomInt();
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.email=email;
+        this.username=username;
+        this.password=password;
+        this.telephone = telephone;
+    }
+
+    public User(int id, String name, String surname, String address, String email, String username, String password, int telephone) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.telephone = telephone;
+    }
+
+    private int randomInt() {
+        double doubleRandom=0;
+
+        doubleRandom=Math.random()*4000;
+
+        int intRandom=(int ) doubleRandom;
+        return intRandom;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User that = (User) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email) && Objects.equals(username, that.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, username);
+    }
 }
