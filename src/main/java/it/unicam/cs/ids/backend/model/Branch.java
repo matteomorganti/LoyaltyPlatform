f9542743dca083c009d1f4a89c1e3a81375d0357
Wallrider09 <56674288+Mak3yrM0v3@users.noreply.github.com>
2023-07-01 21:41:18 +0200

    PointsProgram and Branch implementation

--- src/main/java/it/unicam/cs/ids/backend/model/Branch.java
+++ src/main/java/it/unicam/cs/ids/backend/model/Branch.java
@@ -1,4 +1,26 @@
 package it.unicam.cs.ids.backend.model;
 
 public class Branch {
+
+    private String branchName;
+    private final String address;
+    private final BranchManager owner;
+
+    public Branch(String branchName , String branchAddress, BranchManager owner) {
+        this.branchName = branchName ;
+        this.address = branchAddress;
+        this.owner = owner;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public BranchManager getOwner() {
+        return owner;
+    }
 }
--- src/main/java/it/unicam/cs/ids/backend/model/PointsProgram.java
+++ src/main/java/it/unicam/cs/ids/backend/model/PointsProgram.java
@@ -1,4 +1,44 @@
 package it.unicam.cs.ids.backend.model;
 
-public class PointsProgram {
+public class PointsProgram extends FidelityProgram {
+
+    private int pointXValue;
+    private int totalPoints;
+    public PointsProgram(int id, String name, String description, int pointXValue, int totalPoints) {
+        super(id, name, description);
+        this.pointXValue = pointXValue;
+        this.totalPoints = totalPoints;
+    }
+
+    public PointsProgram(String name, String description) {
+        super(name, description);
+        this.pointXValue =0;
+        this.totalPoints =0;
+    }
+
+    public PointsProgram(String nome, int id) {
+        super(nome, id);
+        this.pointXValue =0;
+        this.totalPoints =0;
+    }
+
+
+    public int getPointXValue() {
+        return pointXValue;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+
+
+    public void setPointXValue(int pointXValue) {
+        this.pointXValue = pointXValue;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
 }
