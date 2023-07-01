6769e10d013703d3449a399350bbc1dce7e187e5
Wallrider09 <56674288+Mak3yrM0v3@users.noreply.github.com>
2023-07-01 21:02:16 +0200

    Database Connection fix
    Implemented database connection and table management methods

--- src/main/java/it/unicam/cs/ids/Main.java
+++ src/main/java/it/unicam/cs/ids/Main.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.ids;
 
+import it.unicam.cs.ids.backend.controller.DBMSController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DBMSController.init();
     }
 }
--- src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
@@ -1,15 +1,52 @@
 package it.unicam.cs.ids.backend.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBMSController {
 
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/LoyaltyPlatform";
-        String user = "pogres";
-        String password = "password";
-        return DriverManager.getConnection(url, user, password);
+        private static String url = "jdbc:postgresql://localhost:5432/LoyaltyPlatform";
+        private static String user = "postgres";
+        private static String password = "password";
+        private static Connection connection;
+
+
+    public static void init() {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected!");
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Not connected", e);
+        }
+    }
+    public static void insertQuery(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+
+    public static void removeQuery(String query) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.executeUpdate();
+    }
+
+    public static ResultSet selectAllFromTable(String table) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table);
+        return statement.executeQuery();
+    }
+
+    public static int getNumberRows(String query) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery(query);
+            if (resultset.last()) {
+                return resultset.getRow();
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            System.out.println("Error getting row count");
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
+
