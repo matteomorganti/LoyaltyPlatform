fedcea33e4d422a98175d39bbc8c5d03aed6df5d
__andrei98 <andreisebastia.stana@studenti.unicam.it>
2023-07-01 21:38:03 +0200

    Update FidelityProgram.java

--- src/main/java/it/unicam/cs/ids/backend/model/FidelityProgram.java
+++ src/main/java/it/unicam/cs/ids/backend/model/FidelityProgram.java
@@ -1,4 +1,44 @@
 package it.unicam.cs.ids.backend.model;
 
 public class FidelityProgram {
+
+    private final String name;
+
+    private String description;
+
+    private final int id;
+
+
+    public FidelityProgram(String name, String description) {
+        this.id= 0;
+        this.name = name;
+        this.description = description;
+    }
+
+    public FidelityProgram(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public FidelityProgram(String name) {
+        this.id=0;
+        this.name = name;
+    }
+
+    public FidelityProgram(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    public  int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
