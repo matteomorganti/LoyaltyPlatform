ddd3c9c88ba14bdc9497cbd47914f8b2038e3648
__andrei98 <andreisebastia.stana@studenti.unicam.it>
2023-07-01 22:33:55 +0200

    Implement PlatformManager class
    The PlatformManager class extends the User class, representing a user with platform management privileges

--- src/main/java/it/unicam/cs/ids/backend/model/PlatformManager.java
+++ src/main/java/it/unicam/cs/ids/backend/model/PlatformManager.java
@@ -1,4 +1,13 @@
 package it.unicam.cs.ids.backend.model;
 
-public class PlatformManager {
+public class PlatformManager extends User {
+
+    private static final int PLATFORM_MEMBERSHIP_COST = 500;
+
+    public PlatformManager(String name, String surname, String address, String email, String username, String password, int telephone) {
+        super(name, surname, address, email, username, password, telephone);
+    }
+    public static int getPlatformMembershipCost() {
+        return PLATFORM_MEMBERSHIP_COST;
+    }
 }
