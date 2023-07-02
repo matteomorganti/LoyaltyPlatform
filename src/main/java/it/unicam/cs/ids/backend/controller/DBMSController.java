da7b79e821926ee6d0cf15a000653e91d93c85b7
Matteo Morganti <matteo.morganti@studenti.unicam.it>
2023-07-03 00:32:33 +0200

    Improve DBMSController class
    Replaced System.out.println with Logger for consistent logging throughout the DBMSController class. Used the try-with-resources statement to automatically close the Statement in a safe manner.

--- src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
@@ -1,49 +1,57 @@
 package it.unicam.cs.ids.backend.controller;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBMSController {
 
-    private static String url = "jdbc:postgresql://localhost:5432/LoyaltyPlatform";
-    private static String user = "postgres";
-    private static String password = "password";
+    private static final String URL = "jdbc:postgresql://localhost:5432/LoyaltyPlatform";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "password";
     private static Connection connection;
 
+    private static final Logger logger = Logger.getLogger(DBMSController.class.getName());
+
+    private DBMSController() {
+    }
 
     public static void init() {
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected!");
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            logger.info("Connected!");
 
         } catch (SQLException e) {
-            throw new IllegalStateException("Not connected", e);
+            logger.log(Level.SEVERE, "Not connected", e);
         }
     }
     public static void insertQuery(String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.executeUpdate(query);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        }
     }
 
     public static void removeQuery(String query) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.executeUpdate();
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.executeUpdate();
+        }
     }
 
     public static ResultSet selectAllFromTable(String table) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table);
-        return statement.executeQuery();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table)) {
+            return statement.executeQuery();
+        }
     }
 
     public static int getNumberRows(String query) {
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultset = statement.executeQuery(query);
+        try (Statement statement = connection.createStatement();
+             ResultSet resultset = statement.executeQuery(query)) {
             if (resultset.last()) {
                 return resultset.getRow();
             } else {
                 return 0;
             }
         } catch (Exception e) {
-            System.out.println("Error getting row count");
+            logger.log(Level.SEVERE, "Error getting row count", e);
             e.printStackTrace();
         }
         return 0;
