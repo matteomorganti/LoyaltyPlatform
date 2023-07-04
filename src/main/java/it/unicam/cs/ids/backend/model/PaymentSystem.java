ca0436c3345c8943c27de995faa81ac40d852afe
Wallrider09 <56674288+Mak3yrM0v3@users.noreply.github.com>
2023-07-04 23:42:10 +0200

    Payment System implementation

--- src/main/java/it/unicam/cs/ids/backend/model/PaymentSystem.java
+++ src/main/java/it/unicam/cs/ids/backend/model/PaymentSystem.java
@@ -1,4 +1,22 @@
 package it.unicam.cs.ids.backend.model;
 
+import it.unicam.cs.ids.backend.controller.PaymentController;
+
+import java.sql.SQLException;
+
 public class PaymentSystem {
+    private TransactionState payment;
+    private final PaymentController paymentState;
+    public PaymentSystem() {
+        paymentState = new PaymentController();
+    }
+    public TransactionState getPayment() {
+        return payment;
+    }
+    public TransactionState verifyPayment(BranchManager t) throws SQLException {
+        if (paymentState.payment(t)){
+            payment = TransactionState.PAID;
+        }else payment = TransactionState.WAITING;
+        return payment;
+    }
 }
