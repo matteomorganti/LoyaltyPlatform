a70f73bdf761043dabc42046414de49cc6c3c0e0
Matteo Morganti <matteo.morganti@studenti.unicam.it>
2023-07-04 20:57:31 +0200

    add payment controller and card controller

--- src/main/java/it/unicam/cs/ids/backend/controller/CardController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/CardController.java
@@ -1,4 +1,79 @@
 package it.unicam.cs.ids.backend.controller;
 
+import it.unicam.cs.ids.backend.model.Branch;
+import it.unicam.cs.ids.backend.model.Customer;
+import it.unicam.cs.ids.backend.model.DateMistake;
+import it.unicam.cs.ids.backend.model.FidelityCard;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardController {
+    private final List<FidelityCard> fidelityCardList;
+
+    public CardController() {
+        this.fidelityCardList = new ArrayList<>();
+    }
+
+    public void addCard(FidelityCard c) throws DateMistake, SQLException {
+        if(findById(c.getId())==null){
+            String query= "INSERT INTO fidelitycard (id_fc, name_fc, expiration_fc, currpoints, currlevel, percentlevel, branch_b, clientid_c ) VALUES('" + c.getId() + "', '" + c.getCardName() + "', '" +c.getExpiration() + "', '" + c.getCurrPoints() + "', '" + c.getCurrLevel() + "', '" + c.getPercentLevel() + "', '" + c.branchCard().getBranchName() + "', '" + c.getClient().getId() + "')";
+            DBMSController.insertQuery(query);
+        }
+        else throw new DateMistake("La carta é gia esistente");
+    }
+
+    public FidelityCard findById(int id) {
+        FidelityCard fidelityCard =null;
+        for (FidelityCard p: this.fidelityCardList){
+            if(p.getId()==id)
+                fidelityCard =p;
+        }
+        if(fidelityCard ==null){
+            return null;
+        }
+        return fidelityCard;
+    }
+
+    /**
+     * Metodo che restituisce
+     * tutte le carte di un singolo cliente;
+     *
+     * @throws SQLException
+     */
+    public void viewFidelityCard(Customer c) throws SQLException, DateMistake {
+        String table="fidelitycard";
+        ResultSet resultSet= DBMSController.selectAllFromTable(table);
+        while (resultSet.next()){
+            if(c.getId()== resultSet.getInt("clientid_c")){
+                RegisterController cr= new RegisterController();
+                cr.viewClients();
+                BranchController cp= new BranchController();
+                cp.viewbranch();
+                Branch addBranch = cp.findById(resultSet.getString("branch_b"));
+                Customer addCustomer = cr.getByID(resultSet.getInt("clientid_c"));
+                FidelityCard cf= new FidelityCard(resultSet.getInt("id_fc"), resultSet.getString("name_fc"),
+                        resultSet.getDate("expiration_fc"), addBranch,
+                        addCustomer, resultSet.getInt("currpoints"),
+                        resultSet.getInt("currlevel"), resultSet.getInt("percentlevel"));
+                this.fidelityCardList.add(cf);
+            }
+        }
+    }
+    @Override
+    public String toString() {
+        String string ="";
+        for (FidelityCard cf : fidelityCardList){
+            string+= "id: ["+ cf.getId()+"] \n" +
+                    "scadenza: ["+ cf.getExpiration()+"] \n" +
+                    "cliente: ["+ cf.getClient().getUsername()+"] \n" +
+                    "puntovendita: ["+cf.branchCard()+"]\n" +
+                    "punticorrenti: ["+cf.getCurrPoints()+"]\n" +
+                    "livellocorrente: ["+cf.getCurrLevel()+"]\n" +
+                    "-------------------------------------------------\n";
+        }
+        return string;
+    }
 }
--- src/main/java/it/unicam/cs/ids/backend/controller/PaymentController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/PaymentController.java
@@ -1,4 +1,61 @@
 package it.unicam.cs.ids.backend.controller;
 
+import it.unicam.cs.ids.backend.model.BranchManager;
+import it.unicam.cs.ids.backend.model.CreditCard;
+import it.unicam.cs.ids.backend.model.DateMistake;
+import it.unicam.cs.ids.backend.model.PlatformManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PaymentController {
+
+    private final List<CreditCard> creditCardList;
+
+    public PaymentController() {
+        this.creditCardList = new ArrayList<>();
+    }
+
+
+
+    /**
+     * se il pagamento è stato effettuato correttamente
+     *
+     * @return true , altrimenti false;
+     */
+
+    public boolean payment(BranchManager t) throws SQLException {
+        if (t.getCard().getCardBalance()> PlatformManager.getPlatformMembershipCost()){
+            t.getCard().decreaseBalance(PlatformManager.getPlatformMembershipCost());
+            return true;
+        }
+        return false;
+    }
+
+    public void addCard(CreditCard cc) throws SQLException {
+        String query = "INSERT INTO creditcard (id_cc, expirationdate, cvv, pin, balance) VALUES('" + cc.getCardNumber() + "','" + cc.getExpirationDate() + "','" + cc.getCvv() + "','" + cc.getPin() + "','" + cc.getCardBalance() + "')";
+        DBMSController.insertQuery(query);
+    }
+
+    public void viewCreditCard() throws SQLException, DateMistake {
+        String table="creditcard";
+        ResultSet resultSet= DBMSController.selectAllFromTable(table);
+        while (resultSet.next()){
+            CreditCard cc= new CreditCard(resultSet.getInt("id_cc"),resultSet.getDate("expirationdate"),
+                    resultSet.getString("cvv"),resultSet.getString("pin"),
+                    resultSet.getDouble("balance"));
+            this.creditCardList.add(cc);
+        }
+    }
+
+    public CreditCard getByID(int id){
+        CreditCard creditCard = null;
+        for (CreditCard cc : creditCardList) {
+            if (cc.getCardNumber() == id)
+                creditCard = cc;
+        }
+        return creditCard;
+    }
 }
