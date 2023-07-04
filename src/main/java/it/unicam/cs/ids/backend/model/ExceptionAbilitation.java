3e770781262963e40c4f6441bf6a92888273ad27
__andrei98 <andreisebastia.stana@studenti.unicam.it>
2023-07-04 14:56:44 +0200

    ADD ExeptionAbilitation

--- src/main/java/it/unicam/cs/ids/backend/model/ExceptionAbilitation.java
+++ src/main/java/it/unicam/cs/ids/backend/model/ExceptionAbilitation.java
@@ -1,4 +1,5 @@
 package it.unicam.cs.ids.backend.model;
 
-public class ExceptionAbilitation extends Throwable {
-}
+public ExceptionAbilitation(String message) {
+        super(message);
+        }
