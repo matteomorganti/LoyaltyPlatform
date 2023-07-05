66fff02261563dfc09d221bc30d65f5de9f7ad25
Wallrider09 <56674288+Mak3yrM0v3@users.noreply.github.com>
2023-07-05 20:17:26 +0200

    DBMS javadocs

--- src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
@@ -4,9 +4,11 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * The DBMSController class handles the connection and operations with the database.
+ */
 public class DBMSController {
 
-    //username db: matteo.morganti99@gmail.com password db: loyaltyplatform2023!
     private static final String URL = "jdbc:postgresql://tyke.db.elephantsql.com:5432/wriuymww";
     private static final String USER = "wriuymww";
     private static final String PASSWORD = "OM8NJJV8KVuOu6rSnEqkdqXhu_RDzpUF";
@@ -16,6 +18,9 @@ public class DBMSController {
     private DBMSController() {
     }
 
+    /**
+     * Initializes the database connection.
+     */
     public static void init() {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -25,6 +30,11 @@ public class DBMSController {
         }
     }
 
+    /**
+     * Executes an INSERT query.
+     *
+     * @param query the INSERT query to execute
+     */
     public static void insertQuery(String query) {
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(query);
@@ -33,6 +43,11 @@ public class DBMSController {
         }
     }
 
+    /**
+     * Executes a REMOVE query.
+     *
+     * @param query the REMOVE query to execute
+     */
     public static void removeQuery(String query) {
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.executeUpdate();
@@ -41,6 +56,12 @@ public class DBMSController {
         }
     }
 
+    /**
+     * Executes a SELECT query and returns the result set.
+     *
+     * @param table the table to select from
+     * @return the result set of the SELECT query
+     */
     public static ResultSet selectAllFromTable(String table) {
         try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table)) {
             return statement.executeQuery();
@@ -50,6 +71,12 @@ public class DBMSController {
         return null;
     }
 
+    /**
+     * Retrieves the number of rows returned by a query.
+     *
+     * @param query the query to execute
+     * @return the number of rows returned by the query
+     */
     public static int getNumberRows(String query) {
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
