4ad01bb3fc8de72a19978b78f3d617ac11390e32
Matteo Morganti <matteo.morganti@studenti.unicam.it>
2023-07-05 22:22:22 +0200

    more optimizations

--- src/test/java/DBMSControllerTest.java
+++ src/test/java/DBMSControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
