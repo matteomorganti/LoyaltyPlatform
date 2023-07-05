cb3e7d41da885c35fcfc57850641691817bbd722
Wallrider09 <56674288+Mak3yrM0v3@users.noreply.github.com>
2023-07-05 19:10:53 +0200

    DBMS connection fix

--- src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/DBMSController.java
@@ -10,48 +10,55 @@ public class DBMSController {
     private static final String USER = "postgres";
     private static final String PASSWORD = "password";
     private static Connection connection;
-
     private static final Logger logger = Logger.getLogger(DBMSController.class.getName());
 
     private DBMSController() {
     }
 
     public static void init() {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
             logger.info("Connected!");
-
         } catch (SQLException e) {
             logger.log(Level.SEVERE, "Not connected", e);
         }
     }
-    public static void insertQuery(String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.executeUpdate(query);
+
+    public static void insertQuery(String query) {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Error executing INSERT query", e);
+        }
     }
 
-    public static void removeQuery(String query) throws SQLException {
+    public static void removeQuery(String query) {
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.executeUpdate();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Error executing REMOVE query", e);
         }
     }
 
-    public static ResultSet selectAllFromTable(String table) throws SQLException {
+    public static ResultSet selectAllFromTable(String table) {
         try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table)) {
             return statement.executeQuery();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Error executing SELECT query", e);
         }
+        return null;
     }
 
     public static int getNumberRows(String query) {
         try (Statement statement = connection.createStatement();
-             ResultSet resultset = statement.executeQuery(query)) {
-            if (resultset.last()) {
-                return resultset.getRow();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            if (resultSet.last()) {
+                return resultSet.getRow();
             } else {
                 return 0;
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             logger.log(Level.SEVERE, "Error getting row count", e);
-            e.printStackTrace();
         }
         return 0;
     }
