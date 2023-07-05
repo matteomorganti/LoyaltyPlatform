0c3646c23fce6b3935df250bd3ac50a8a0e9fa1a
__andrei98 <andreisebastia.stana@studenti.unicam.it>
2023-07-05 19:29:27 +0200

    add Javadoc to RegisterController class

--- src/main/java/it/unicam/cs/ids/backend/controller/RegisterController.java
+++ src/main/java/it/unicam/cs/ids/backend/controller/RegisterController.java
@@ -24,18 +24,39 @@ public class RegisterController {
         this.paymentSystem = new PaymentSystem();
     }
 
+    /**
+     * Retrieves the list of registered customers.
+     *
+     * @return The list of registered customers.
+     */
     public List<Customer> getCustomers() {
         return customers;
     }
 
+    /**
+     * Retrieves the list of registered branch owners.
+     *
+     * @return The list of registered branch owners.
+     */
     public List<BranchManager> getBranchOwners() {
         return branchOwners;
     }
 
+    /**
+     * Retrieves the list of registered cashiers.
+     *
+     * @return The list of registered cashiers.
+     */
     public List<Cashier> getCashierList() {
         return cashierList;
     }
 
+    /**
+     * Registers a new branch owner.
+     *
+     * @param owner The branch owner to register.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public void ownerRegistration(BranchManager o) throws SQLException {
         if (isDataValid(o)) {
             String query = "INSERT INTO owners (id_o, name_o, surname_o, address_o, email_o, username_o, password, enabled, phonenumber_o) VALUES('" + o.getId() + "','" + o.getName() + "','" + o.getSurname() + "','" + o.getAddress() + "','" + o.getEmail() + "','" + o.getUsername() + "' ,'" + o.getPassword() + "' ,'" + o.isActive() + "', '" + o.getTelephone() + "' )";
@@ -43,6 +64,14 @@ public class RegisterController {
         }
     }
 
+    /**
+     * Adds a registered branch owner to the system.
+     * Verifies the owner's payment and updates their enabled status accordingly.
+     *
+     * @param owner The branch owner to add.
+     * @throws SQLException   If an SQL exception occurs.
+     * @throws DateMistake    If there is an issue with the payment date.
+     */
     public void addOwner(BranchManager owner) throws SQLException, DateMistake {
         if (paymentSystem.verifyPayment(owner) == TransactionState.PAID) {
             String query = "UPDATE owners SET enabled = 'true' WHERE id_t = '" + owner.getId() + "'";
@@ -52,6 +81,14 @@ public class RegisterController {
         }
     }
 
+    /**
+     * Finds a branch owner by their ID.
+     *
+     * @param id The ID of the branch owner to find.
+     * @return The branch owner if found.
+     * @throws SQLException   If an SQL exception occurs.
+     * @throws DateMistake    If there is an issue with the payment date.
+     */
     public BranchManager findById(int id) throws SQLException, DateMistake {
         BranchManager owner = null;
         for (BranchManager t : getAllRetailers()) {
@@ -65,10 +102,10 @@ public class RegisterController {
     }
 
     /**
-     * metodo per controllare se i dati inseriti sono corretti
+     * Checks if the provided user data is valid.
      *
-     * @param user
-     * @return true se i dati sono corretti, false altrimenti.
+     * @param user The user to validate.
+     * @return True if the data is valid, false otherwise.
      */
     private boolean isDataValid(User user) {
         if (user.getName() == null || user.getEmail() == null || user.getUsername() == null || user.getPassword() == null) {
@@ -77,6 +114,13 @@ public class RegisterController {
         return true;
     }
 
+    /**
+     * Retrieves all registered branch owners from the database.
+     *
+     * @return The list of registered branch owners.
+     * @throws SQLException   If an SQL exception occurs.
+     * @throws DateMistake    If there is an issue with the payment date.
+     */
     public List<BranchManager> getAllRetailers() throws SQLException, DateMistake {
         String retailer = "retailers";
         ResultSet resultset = DBMSController.selectAllFromTable(retailer);
@@ -95,6 +139,12 @@ public class RegisterController {
         return branchOwners;
     }
 
+    /**
+     * Registers a new customer.
+     *
+     * @param customer The customer to register.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public void customerRegistration(Customer customer) throws SQLException {
         if (isDataValid(customer)) {
             String query = "INSERT INTO customers (id_c, name_c, surname_c, address_c, email_c, username_c, password_c, phonenumber_c) VALUES('" + customer.getId() + "','" + customer.getName() + "','" + customer.getSurname() + "','" + customer.getAddress() + "','" + customer.getEmail() + "','" + customer.getUsername() + "' ,'" + customer.getPassword() + "' ,'" + customer.getTelephone() + "' )";
@@ -103,6 +153,12 @@ public class RegisterController {
 
     }
 
+    /**
+     * Retrieves the list of registered customers from the database.
+     *
+     * @return The list of registered customers.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public List<Customer> viewCustomers() throws SQLException {
         String table="customers";
         ResultSet resultSet= DBMSController.selectAllFromTable(table);
@@ -116,6 +172,13 @@ public class RegisterController {
         return customers;
     }
 
+    /**
+     * Finds a customer by their ID.
+     *
+     * @param id The ID of the customer to find.
+     * @return The customer if found, null otherwise.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public Customer getByID(int id) throws SQLException {
         viewCustomers();
         for(Customer c: this.customers){
@@ -125,6 +188,12 @@ public class RegisterController {
         return null;
     }
 
+    /**
+     * Registers a new cashier for a branch.
+     *
+     * @param cashier The cashier to register.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public void branchCashierRegistration(Cashier cashier) throws SQLException {
         if (isDataValid(cashier)) {
             String query = "INSERT INTO cashier (id_cs, name_cs, surname_cs, address_cs, email_cs, username_cs, password, phoneNumber_cs, branch_b) VALUES('" + cashier.getId() + "','" + cashier.getName() + "','" + cashier.getSurname() + "','" + cashier.getAddress() + "','" + cashier.getEmail() + "','" + cashier.getUsername() + "' ,'" + cashier.getPassword() + "' ,'" + cashier.getTelephone() + "', '" + cashier.getBranch().getBranchName() + "' )";
@@ -133,6 +202,13 @@ public class RegisterController {
 
     }
 
+    /**
+     * Retrieves the list of registered cashiers from the database.
+     *
+     * @return The list of registered cashiers.
+     * @throws SQLException If an SQL exception occurs.
+     * @throws DateMistake  If there is an issue with the payment date.
+     */
     public List<Cashier> viewCashiers() throws SQLException, DateMistake {
         String table="cashiers";
         ResultSet resultSet= DBMSController.selectAllFromTable(table);
@@ -150,6 +226,14 @@ public class RegisterController {
         return this.cashierList;
     }
 
+    /**
+     * Finds a cashier by their ID.
+     *
+     * @param id The ID of the cashier to find.
+     * @return The cashier if found, null otherwise.
+     * @throws SQLException If an SQL exception occurs.
+     * @throws DateMistake  If there is an issue with the payment date.
+     */
     public Cashier getById(int id) throws SQLException, DateMistake {
         viewCashiers();
         for(Cashier cs: this.cashierList){
@@ -159,12 +243,23 @@ public class RegisterController {
         return null;
     }
 
+    /**
+     * Updates the credit card information for a branch owner.
+     *
+     * @param owner The branch owner to update.
+     * @param cc    The credit card to set.
+     * @throws SQLException If an SQL exception occurs.
+     */
     public void cardUpdate(BranchManager o, CreditCard cc) throws SQLException {
         String query="UPDATE owners SET ccid_cc = '" + cc.getCardNumber() + "' WHERE id_o = '" + o.getId() + "'";
         DBMSController.insertQuery(query);
     }
-
-
+    
+    /**
+     * Retrieves a string representation of the registered customers.
+     *
+     * @return A string representation of the registered customers.
+     */
     public String toStringCustomers() {
         String string ="";
         for (Customer customer : customers){
